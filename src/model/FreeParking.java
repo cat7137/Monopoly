c17cb8f6a19746be42ed7f17c0c126272c8da235
cat7137 <cat7137@rit.edu>
2023-12-24 01:01:25 -0500

    implemented Free Parking

--- src/model/FreeParking.java
+++ src/model/FreeParking.java
@@ -2,9 +2,24 @@ package model;
 
 public class FreeParking extends Tiles{
 
+    private int moneyInMiddle;
+
     public FreeParking(String name, int cost, int rent, int mortgage, Colors color) {
         super(name, cost, rent, mortgage, color);
+        moneyInMiddle = 0;
         
+    }
+
+    public int getMoneyInMiddle(){
+        return this.moneyInMiddle;
+    }
+
+    public void collectMoney(Player player){
+        player.addMoney(moneyInMiddle);
+    }
+
+    public void setMoneyInMiddle(int amount){
+        this.moneyInMiddle += amount;
     }
     
 }
