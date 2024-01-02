c7730d8f5983576fe0576ee9094885fc620f5d17
cat7137 <cat7137@rit.edu>
2024-01-01 23:02:36 -0500

    implemented GO space

--- src/model/Go.java
+++ src/model/Go.java
@@ -5,5 +5,9 @@ public class Go extends Tiles {
     public Go(String name, int cost, int rent, int mortgage, Colors color) {
         super(name, cost, rent, mortgage, color);
     }
+
+    public void addMoney(Player player){
+        player.addMoney(200);
+    }
     
 }
